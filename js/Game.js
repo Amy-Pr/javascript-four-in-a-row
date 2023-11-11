bbabd56aad2fca883da35943a076167db1462c58
amy_pr <ampreci@gmail.com>
2023-11-11 15:59:34 -0800

    add class files

---
+++ js/Game.js
@@ -0,0 +1,19 @@
+//What should be objects? 
+
+//The game pieces, red vs. yellow as separate objects
+//The user interacts by moving them with arrows and dropping them in a column
+//Their position on the x axis will change one unit based on user input, left vs. right
+//Should the playing table be an object?
+//When player hits enter, the current piece falls to the bottom of the grid (Y position of 0)
+//Then automatically switches color to next turn?
+//If this were React, state would be the position and color of the current piece
+
+//Winning: Could we number each circle on the grid and use conditionals to determine winner? T/F for if there is a row with the same color?
+//Not sure how I would do that.
+
+//CLASSES: GAME, PLAYER, BOARD, SPACES, TOKEN
+
+class Game {
+
+    
+}

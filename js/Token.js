825661a6672adfd4221190d770358f1c6273a6c5
amy_pr <ampreci@gmail.com>
2023-11-11 16:17:31 -0800

    initialize token class properties

--- js/Token.js
+++ js/Token.js
@@ -1,4 +1,9 @@
+//possible properties: color, id matching player id, position?, status: played t/f
 class Token {
-
+    constructor() {
+        this.owner = this.owner; 
+        this.id = `token-${index}-${owner-id}`;
+        this.dropped = false;
+    }
     
 }

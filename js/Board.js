9d8659ffc57a14e44e3ad3b8cfa70508473a9613
amy_pr <ampreci@gmail.com>
2023-11-20 13:57:36 -0800

    set up constructors for Board and Space classes

--- js/Board.js
+++ js/Board.js
@@ -1,4 +1,10 @@
 class Board {
+    constructor() { //Don't need arguments; we are statically setting the board size.
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = [];
+
+    }
 
     
 }
--- js/Space.js
+++ js/Space.js
@@ -1,4 +1,10 @@
 class Space {
-
+    constructor(x, y, id, token) {
+        this.x = x;
+        this.y = y;
+        this.token = null;
+        this.id = `space-${x}-${y}`;
+        
+    }
     
 }

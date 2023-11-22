25d15a5245a290ea1ad454b161e15c861afc67bf
amy_pr <ampreci@gmail.com>
2023-11-22 13:10:36 -0800

    draw the token using render method

--- js/Token.js
+++ js/Token.js
@@ -6,4 +6,23 @@ class Token {
         this.dropped = false;
     }
     
+    /** 
+     * Gets associated htmlToken.
+     * @return  {element}   Html element associated with token object.
+     */
+    get htmlToken() {
+        return document.getElementById(this.id);
+    }
+
+
+    drawHTMLToken () {
+        const token = document.createElement('div');
+        document.querySelector('#game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class', 'token');
+        token.style.backgroundColor = this.owner.color; //accessing the color property in Player object
+        
+    }
+
+    
 }
